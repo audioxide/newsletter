1f136f8654253e4a3852de5ceb367541e293fac1
Frederick O'Brien <frederik.obrien@gmail.com>
2021-06-12 22:22:46 +0100

    Script writes to HTML file
    Primitive version of full chain now up and running. The script fetches JSON data, filters it for content published in a given date range, and writes the results to a HTML file

--- generator/generateNewsletter.js
+++ generator/generateNewsletter.js
@@ -1,44 +1,75 @@
 const fetch = require("node-fetch");
+const fs = require('fs')
+const path = require('path')
 
 // Set date range
 
 const from = new Date("2021-03-31").getTime();
 const to = new Date("2021-05-01").getTime();
 
-// Check for albums in time range
+const reviewsJson = 'https://api.audioxide.com/reviews.json';
+const articlesJson = 'https://api.audioxide.com/articles.json';
+const interviewsJson = 'https://api.audioxide.com/interviews.json';
+const listeningPartiesJson = 'https://api.audioxide.com/listening-parties.json';
+const funnyfarmJson = 'https://api.audioxide.com/funnyfarm.json';
 
-fetch('https://api.audioxide.com/reviews.json')
+buildNewsletter().then(res => writeToFile(res));
+
+async function buildNewsletter() {
+
+  var newsletterHtml = "<!-- HEADER -->\n\n";
+
+  // Check for and add reviews in time range
+
+  await fetch(reviewsJson)
   .then(response => response.json())
   .then(data => {
     let tally = data.length - 1;
-    console.log("Albums reviewed in time range");
+    newsletterHtml = newsletterHtml.concat("<!-- ALBUM REVIEWS -->\n\n")
     for (let i = tally; i > 0; i--) {
       var publishDate = new Date(data[i].metadata.created).getTime();
       if (publishDate >= from && publishDate <= to) {
-        console.log("‘" + data[i].metadata.album + "’ by " + data[i].metadata.artist);
+        newsletterHtml = newsletterHtml.concat("‘" + data[i].metadata.album + "’ by " + data[i].metadata.artist + "\n")
       }
     }
-    console.log("\n");
+    newsletterHtml = newsletterHtml.concat("\n")
   });
+  
+  // Check for and add articles in time range
 
-// Check for articles in time range
-
-fetch('https://api.audioxide.com/articles.json')
+  await fetch(articlesJson)
   .then(response => response.json())
   .then(data => {
     let tally = data.length - 1;
-    console.log("Articles published in time range");
+    newsletterHtml = newsletterHtml.concat("<!-- ARTICLES -->\n\n");
     for (let i = tally; i > 0; i--) {
       var publishDate = new Date(data[i].metadata.created).getTime();
       if (publishDate >= from && publishDate <= to) {
-        console.log("‘" + data[i].metadata.title + "’ by " + data[i].metadata.author.name);
+        newsletterHtml = newsletterHtml.concat("‘" + data[i].metadata.title + "’ by " + data[i].metadata.author.name + "\n");
       }
     }
-    console.log("\n");
+    newsletterHtml = newsletterHtml.concat("\n")
   });
 
-// Check for interviews in time range
+  // Check for and add interviews in time range
 
-// Check for listening parties in time range
+  // Check for and add listening parties in time range
 
-// Check for Funnyfarm entries in time range
+  // Check for and add Funnyfarm entries in time range
+
+  // Add album artwork stories template
+
+  newsletterHtml = newsletterHtml.concat("<!-- ARTWORK STORIES -->\n\n")
+
+  // Add site development template
+
+  newsletterHtml = newsletterHtml.concat("<!-- SITE DEVELOPMENT -->\n")
+
+  return newsletterHtml;
+
+};
+
+function writeToFile(html) {
+    const filePath = path.join(__dirname, '/test.html');
+    fs.writeFileSync(filePath, html);
+};
